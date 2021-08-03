d5851f1195bb52a74d3518569f3303075b5fa0ae
Jinjia2 <62843050+MASTER-12345@users.noreply.github.com>
2021-08-03 15:05:01 +0800

    Create creatbud(创建建筑，无法判断红色)1.cs

---
+++ RTS_Game/creatbud(创建建筑，无法判断红色)1.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class building_Script : MonoBehaviour
+{
+    public GameObject Buiding1;
+
+    public GameObject moniBuiding1;
+
+    private bool take = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (take == true)
+        {
+            tas();
+
+        }
+    }
+    public void instanceBuiding()
+    {
+        print("出现建筑！");
+
+        take = true;
+        moniBuiding1.SetActive(true);
+       
+    }
+    public void tas()
+    {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100f))        //最大长度设置为100f
+        {
+            moniBuiding1.transform.position = hit.point;
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Instantiate(Buiding1, hit.point, Quaternion.identity);
+                take = false;
+            }
+        }
+
+        
+
+    }
+}
