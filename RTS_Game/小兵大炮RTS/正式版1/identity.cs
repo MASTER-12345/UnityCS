4ed25483c69e88d1d84bce416b912b9cc8cb62df
Jinjia2 <62843050+MASTER-12345@users.noreply.github.com>
2021-10-18 19:43:04 +0800

    Create identity.cs

---
+++ RTS_Game/小兵大炮RTS/正式版1/identity.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Identity : MonoBehaviour
+{
+    public enum identity_
+    {
+        red,yellow,blue,green
+    }
+
+    public identity_ my_identity = identity_.red;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
