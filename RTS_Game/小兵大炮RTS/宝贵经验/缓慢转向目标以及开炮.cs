ce7c1b142d6dc5139e1548d0e53c353b1cb17a69
Jinjia2 <62843050+MASTER-12345@users.noreply.github.com>
2021-10-19 11:49:54 +0800

    Update and rename 缓慢转向目标.cs to 缓慢转向目标以及开炮.cs

--- RTS_Game/小兵大炮RTS/宝贵经验/缓慢转向目标以及开炮.cs
+++ RTS_Game/小兵大炮RTS/宝贵经验/缓慢转向目标以及开炮.cs
@@ -1,4 +1,11 @@
         Quaternion shotRotation = Quaternion.LookRotation(returnPositon.transform.position - Paota.transform.position);
         Paota.transform.rotation = Quaternion.Lerp(Paota.transform.rotation, shotRotation, 0.5f * Time.deltaTime);
         Paota.transform.localEulerAngles = new Vector3(0, Paota.transform.localEulerAngles.y, 0);
+        //确定两者之间角度，如果对准了，就开炮
+          //print("两者角度" + Vector3.Angle(vec,Paotaa.transform.forward));
+            if (Vector3.Angle(vec, Paotaa.transform.forward) < 7)
+            {
+                print("开炮");
+            }
+
 
