f9a8e34b7a92562be7a443fede68a6ee8d0af495
Jinjia2 <62843050+MASTER-12345@users.noreply.github.com>
2022-07-07 17:27:38 +0800

    Update 检测是否点击到Ui.cs

--- RTS_Game/2022Lot项目/检测是否点击到Ui.cs
+++ RTS_Game/2022Lot项目/检测是否点击到Ui.cs
@@ -1,5 +1,23 @@
 
-//第一种方法，最简单实用
+
+//首选！！！！！！！第三种方法通过画布上的GraphicRaycaster组件发射射线进行检测
+
+public bool IsPointerOverUIObject(Canvas canvas, Vector2 screenPosition) 
+{
+    PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+    eventDataCurrentPosition.position = screenPosition;
+    GraphicRaycaster uiRaycaster = canvas.gameObject.GetComponent<GraphicRaycaster>();
+    
+    List<RaycastResult> results = new List<RaycastResult>();
+    uiRaycaster.Raycast(eventDataCurrentPosition, results);
+    return results.Count > 0;
+}
+
+
+
+
+
+//不推荐，不实用
 if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) 
 {
     //TODO：点击在了UI上
@@ -27,18 +45,6 @@ public bool IsPointerOverUIObject(Vector2 screenPosition)
 }
 
 
-//第三种方法通过画布上的GraphicRaycaster组件发射射线进行检测
-
-public bool IsPointerOverUIObject(Canvas canvas, Vector2 screenPosition) 
-{
-    PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
-    eventDataCurrentPosition.position = screenPosition;
-    GraphicRaycaster uiRaycaster = canvas.gameObject.GetComponent<GraphicRaycaster>();
-    
-    List<RaycastResult> results = new List<RaycastResult>();
-    uiRaycaster.Raycast(eventDataCurrentPosition, results);
-    return results.Count > 0;
-}
 -----------------------------------
 ©著作权归作者所有：来自51CTO博客作者mb60e5417d375b8的原创作品，请联系作者获取转载授权，否则将追究法律责任
 Unity中解决IsPointerOverGameObject防UI穿透在移动端检测失败
