a0d59447526c2f066fd8c4cf7bcf39cff89ea6b3
Jinjia2 <62843050+MASTER-12345@users.noreply.github.com>
2023-01-06 20:43:19 +0800

    Update and rename Windows滚轮缩放.cs to Windows滚轮缩放以及旋转.cs

--- RTS_Game/Windows滚轮缩放以及旋转.cs
+++ RTS_Game/Windows滚轮缩放以及旋转.cs
@@ -11,5 +11,13 @@ void Zoom()
             transform.position = transform.position + new Vector3(0, scrollspeed, 0);
 
         }
+         if (Input.GetKey(KeyCode.Q))
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, 0.1f, 0));
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles - new Vector3(0, 0.1f, 0));
+        }
     }
     //update里调用此函数
