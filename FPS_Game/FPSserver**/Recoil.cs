12c383ffb70e64f3779fb9416af32b5bc3e98d99
Jinjia2 <62843050+MASTER-12345@users.noreply.github.com>
2023-02-06 14:34:10 +0800

    Create Recoil.cs

---
+++ FPS_Game/FPSserver**/Recoil.cs
@@ -0,0 +1,44 @@
+//可实现垂直水平后坐力以及其回归
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse : MonoBehaviour
+{
+    float h;
+    float v;
+    public float initTime;
+     float fireTime=0.1f;
+
+    float hou;
+    float shui;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h += Input.GetAxis("Mouse X");
+        v += Input.GetAxis("Mouse Y");
+
+        hou = Mathf.MoveTowards(hou, 0, 0.03f);//将后坐力降至0,回归
+        shui = Mathf.MoveTowards(shui, 0, 0.03f);
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Time.time - initTime > fireTime)
+            {
+                print("23");
+                initTime = Time.time;
+                hou = hou + 3.5f;//添加后坐力
+                shui = shui + Random.RandomRange(-8, 8);
+            }
+            
+        }
+        transform.localEulerAngles = new Vector3 (-(v + hou), h+shui, 0);
+    }
+
+}
